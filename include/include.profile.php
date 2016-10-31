0846673e88be504a6a115ec3be700c2ac50c9dce
Leomar Bucud <leomarbucud@gmail.com>
2016-10-31 23:46:47 +0800

    include status box (include file) on profile page

--- include/include.profile.php
+++ include/include.profile.php
@@ -79,35 +79,7 @@ $posts = getAllPosts($uid);
                 <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas"><i class="glyphicon glyphicon-chevron-left"></i></button>
             </p>
             <div class="profile-page">
-                <div class="status-box">
-                    <div class="media">
-                        <div class="media-left">
-                            <img class="media-object img-circle" src="<?=$config['url']['base_path']?>/assets/images/uploads/profiles/<?=$s->_get('user')['profile']?>" width="50" height="50" />
-                        </div>
-                        <div class="media-body">
-                            <form id="status-form" action="<?=$config['url']['base_path']?>/action/action.upload.php" enctype="multipart/form-data" data-toggle="validator" role="form">
-                                <input type="hidden" name="lat" id="lat" />
-                                <input type="hidden" name="lng" id="lng" />
-                                <input type="hidden" name="loc" id="loc" />
-                                <div class="panel panel-default">
-                                    <div class="image-preview hide"></div>
-                                    <div class="panel-body">
-                                        <textarea class="box" id="text" name="text" placeholder="Share your travels..." required></textarea>
-                                        <label class="my-location"><span class="glyphicon glyphicon-map-marker"></span> <span class="location">Looking where you are...</span></label>
-                                    </div>
-                                    <div class="panel-footer">
-                                        <span class="btn btn-default btn-sm btn-file">
-                                            <span class="glyphicon glyphicon-camera"></span>
-                                            <input type="file" name="postImg" id="post-image" required/>
-                                        </span>
-                                        <button type="submit" class="btn btn-success btn-sm pull-right">Post</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <hr>
-                </div>
+                <?php include_once 'include/include.status.box.php'; ?>
                 <div class="post-list">
                     <?php foreach($posts as $post): ?>
                     <div class="media post <?=$post['isApproved'] == 0 ? 'alert alert-warning':''?>" data-post-id="<?=$post['post_id']?>">
