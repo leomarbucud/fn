ef684861a1233355e049fe2cb8978ab5604294df
Leomar Bucud <leomarbucud@gmail.com>
2017-02-09 00:51:19 +0800

    Update terms and conditions

--- include/include.terms.php
+++ include/include.terms.php
@@ -6,10 +6,10 @@
 					<h1>TERMS AND CONDITION</h1>
 					<p class="lead">You agree to the terms and conditions, in these terms of agreement with respect to our site you fully understand that the said below was to follow between you and the agency.</p>
 					<ol type="1">
-						<li>Customer can cancel or rebooked ticket while the payment has not been made</li>
-						<li>Customer can pay cash or credit card</li>
-						<li>Using credit card can be charge to 2 percent  with the total</li>
-						<li>Rebooking ticket has a charge of 1000 pessos</li>
+						<li>Customer can cancel or rebook their tickets while the payment is pending.</li>
+						<li>Customer can pay through cash or credit card.</li>
+						<li>Using a credit card will be charged 2%.</li>
+						<li>Rebooking a ticket has a charge of 1000 pesos.</li>
 					</ol>
 				</div>
 				<div class="col-md-4">
